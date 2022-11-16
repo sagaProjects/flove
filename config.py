74e6ac2a37a745657e2f652b46482670e92542a7
I am Saga <100592571+sagaProjects@users.noreply.github.com>
2022-11-17 00:13:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -33,14 +33,12 @@ DB_URI = os.environ.get("DATABASE_URL", "postgres://obhgjqqm:H7DBFEyfQEEIbzqrwzo
 # Username CH & Group
 CHANNEL = os.environ.get("CHANNEL", "ASUPAN BELLEZA")
 GROUP = os.environ.get("GROUP", "ʙᴇʟʟᴇᴢᴀ-ɴʏᴇɴʏᴇ⎊225")
-CHANNEL 1 = os.environ.get("CHANNEL1", "ʀɪɴᴛɪʜᴀɴ ʜᴀᴛɪ
-")
+CHANNEL1 = os.environ.get("CHANNEL1", "ʀɪɴᴛɪʜᴀɴ ʜᴀᴛɪ")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001327370766"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001474035788"))
-FORCE_SUB_CHANNEL 1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1001541395503
-"))
+FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1001541395503"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
