f538052df1dc59e6a0b2afa703813bc108f9eb64
I am Saga <100592571+sagaProjects@users.noreply.github.com>
2022-11-17 01:26:01 +0700

    Update bot.p

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from config import (
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
-    FORCE_SUB_CHANNEL1,
+    LOGGER,
     OWNER,
     TG_BOT_TOKEN,
     TG_BOT_WORKERS,
@@ -32,6 +32,7 @@ class Bot(Client):
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN,
         )
+        self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
@@ -40,6 +41,7 @@ class Bot(Client):
         if FORCE_SUB_CHANNEL:
             try:
                 link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                self.invitelink = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning(
@@ -49,7 +51,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/helpforRYUU untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/scattersuport untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_GROUP:
@@ -65,22 +67,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/helpforRYUU untuk Bantuan"
-                )
-        if FORCE_SUB_CHANNEL1:
-            try:
-                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
-                self.invitelink = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_CHANNEL1!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL1}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/helpforRYUU untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/scattersuport untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -94,14 +81,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/helpforRYUU untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/scattersuport untuk Bantuan"
             )
             sys.exit()
 
-       
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/RYUUSHINNI"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/scattersuport"
         )
         self.username = usr_bot_me.username
 
