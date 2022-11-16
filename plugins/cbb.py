ef8550549c09ffa9f8f45f4bfccfa86dafb00508
I am Saga <100592571+sagaProjects@users.noreply.github.com>
2022-11-17 01:32:30 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • 𝘽𝘼𝘾𝘼 𝙆𝙊𝙈𝙄𝙆 𝙃𝙀𝙉𝙏𝙄 >: @Doujindesukomik\n • 𝘾𝙃𝘼𝙉𝙉𝙀𝙇 𝙍𝘼𝙍𝙀 𝙆𝙊𝙇𝙀𝙆𝙎𝙄 >: @RareCollectionID\n • 𝙆𝙊𝙇𝙀𝙆𝙎𝙄 𝙇𝙊𝙆𝘼𝙇 >: @LokalPrideid\n • 𝙑𝙄𝘿𝙀𝙊 𝙑𝙄𝙍𝘼𝙇 >: <a href='https://t.me/viralbelatungid'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • 𝐃𝐄𝐕 >: @thismebots\n • 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 >: @scatteesuport\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
